98116fa43a1faed804587dba2c6edf2b8a87a61b
YoniZangolsaz <yoniza770@gmail.com>
2024-09-18 00:27:43 +0300

    add login func

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ const app = express();
 const uri = "mongodb+srv://israel:Israel211@cluster0.qvg0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new mongoDb.MongoClient (uri);
 
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }));
+app.use(express.json()); 
+
 
 app.get("/api/boards", async(req, res) => {
     try {
@@ -21,6 +23,56 @@ app.get("/api/boards", async(req, res) => {
     } catch (err) {
         console.error("big pro lol", err);
         res.status(500).json({ error: "There is error" });
+    } finally {
+        await client.close();
+    }
+
+});
+
+app.post("/api/signup", async(req, res) => {
+    try {
+        console.log(req.body);
+        
+        userNmae = req.body.name
+        password = req.body.password
+
+        console.log("try is good");
+        
+        await client.connect();
+        const db = client.db("test")
+        const col = db.collection("users")
+        const user = await col.insertOne({userNmae, password});
+        res.json({ user });
+    } catch (err) {
+        console.error("big pro lol", err);
+        res.status(500).json({ error: err });
+    } finally {
+        await client.close();
+    }
+
+});
+
+app.post("/api/login", async(req, res) => {
+    try {
+        console.log(req.body);
+        
+        userNmae = req.body.name
+        password = req.body.password
+
+        console.log("try is good");
+        
+        await client.connect();
+        const db = client.db("test")
+        const col = db.collection("users")
+        const user = await col.findOne({userNmae, password});
+        if (!user) {
+            res.status(401).json({ error: "User not found" });
+            return;
+        }
+        res.json({ user });
+    } catch (err) {
+        console.error("big pro lol", err);
+        res.status(500).json({ error: err });
     } finally {
         await client.close();
     }
